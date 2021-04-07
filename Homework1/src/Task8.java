f31cb0ab77d5b5a7d5a062bdfbccbccb0782ddce
Olesia Meleshko <omeleshko11@gmail.com>
2021-04-07 18:09:10 +0300

    Change variable name

--- Homework1/src/Task8.java
+++ Homework1/src/Task8.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class Task8 {
         public static void main (String[] args) {
-        double[ ] array_double = {1.1, 1.2, 1.3, 1.4, 1.5};
-        double first = array_double[0];
-        double last = array_double[array_double.length -1];
+        double[ ] arrayDouble = {1.1, 1.2, 1.3, 1.4, 1.5};
+        double first = arrayDouble[0];
+        double last = arrayDouble[arrayDouble.length -1];
             System.out.println(first + ", " + last);
         }
 }
