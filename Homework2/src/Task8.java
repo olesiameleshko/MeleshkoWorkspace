11458a35e39f08b4f532036e93d96e12cadff7c1
Olesia Meleshko <omeleshko11@gmail.com>
2021-04-20 11:29:33 +0300

    Added Homework2. Changed gitignore

--- Homework2/src/Task8.java
+++ Homework2/src/Task8.java
@@ -6,38 +6,3 @@ public class Task8 {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-        //for (int i = 0; i < 100; i++)
-        // if (i % 2 != 0)
-        //if (i == 3) break;
-        //if (i == 5) break;
-        //System.out.println(i + "");
-        /*
-        for (int i = 0; i < 100; i++) {
-
-            if (i % 3 == 0)
-            System.out.println(i + " ");
-        }
-        for (int i = 0; i < 100; i++) {
-            if (i % 5 == 0)
-            System.out.println(i + "");
-        }
-        for (int i = 0; i < 100; i++) {
-            if (i % 2 != 0)
-            System.out.println(i + "");
-        }
-
-       System.out.println();
-    }
-}
-*/
